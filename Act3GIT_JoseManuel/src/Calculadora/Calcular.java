42afa5e44eb732c5e6bfa7fb402b2b3b9f56491a
Jromgom <jromgom0507@g.educaand.es>
2024-11-27 19:36:11 +0100

    cambio el programa a 30 números

--- Act3GIT_JoseManuel/src/Calculadora/Calcular.java
+++ Act3GIT_JoseManuel/src/Calculadora/Calcular.java
@@ -1,51 +1,19 @@
 package Calculadora;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Calcular {
 
 	public static void main(String[] args) {
-		
-		//1º Informamos
-		System.out.println("Este programa realiza una calculadora simple");
-		
-		//2º Pedimos los valores
-		Scanner entrada = new Scanner(System.in);
-	
-		System.out.println("Introduzca el primer valor: ");
-		double primerValor = Double.parseDouble(entrada.nextLine());
-		// Pedir la operación a realizar. Los valores admitidos serían +, -, *, / y %
-		System.out.println("Introduce la operación a realizar: ");
-		char operacion = entrada.nextLine().charAt(0);		
-		System.out.println("Introduzca el segundo valor: ");
-		double segundoValor = entrada.nextDouble();
-		
-		//3º Resolvemos
-		
-		
-		if(operacion == '+') {
-			double resultado = primerValor + segundoValor;
-			System.out.println("El resultado de la suma es: " + resultado);
-		}else if(operacion == '-') {
-			double resultado = primerValor - segundoValor;
-			System.out.println("El resultado de la resta es: " + resultado);
-		}else if(operacion == '*') {
-			double resultado = primerValor * segundoValor;
-			System.out.println("El resultado de la multiplicación es: " + resultado);
-		}else if(operacion == '/') {
-			if (segundoValor != 0) {
-				double resultado = primerValor / segundoValor;
-				System.out.println("El resultado de la división es: " + resultado);
-			}else {
-				System.out.println("ERROR: Introduzca un valor distinto de cero");
-			}
-		}else if(operacion == '%') {
-			if (segundoValor != 0) {
-				double resultado = primerValor % segundoValor;
-				System.out.println("El resultado de la división es: " + resultado);				
-			}else {
-				System.out.println("ERROR: Introduzca un valor distinto de cero");
-			}
-		}	
-	}
+        generarNumerosAleatorios();
+    }
+
+    public static void generarNumerosAleatorios() {
+        Random random = new Random(); 
+        System.out.println("30 números aleatorios entre 1 y 10:");
+        for (int i = 0; i < 30; i++) { 
+            int numeroAleatorio = random.nextInt(10) + 1; 
+            System.out.print(numeroAleatorio + " ");
+        }
+    }
 }
